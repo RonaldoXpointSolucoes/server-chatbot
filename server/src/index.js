cb49789cf62cdff3c9e43cda274f5c489c74b0e6
X-Point Soluções Tecnológicas <36340488+xpointsolucoes@users.noreply.github.com>
2026-04-08 20:59:15 -0300

    fix(cors): permit custom headers for api gateway

--- server/src/index.js
+++ server/src/index.js
@@ -44,8 +44,12 @@ process.on('unhandledRejection', (reason, promise) => {
     console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-tenant-id', 'apikey']
+}));
 app.use(helmet());
-app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
